da3224b5ecac0b2cc08fee068506754a874348f8
Prathamesh Kokane <100931667+prathameshkokane@users.noreply.github.com>
2023-10-07 17:36:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Circle from "./assets/Circle.png";
 import "./styles/app.scss";
 
 function App() {
   return (
     <>
+      <img className="circle" src={Circle} alt="" />
       <Navbar />
       <Hero />
       <AboutUs />
